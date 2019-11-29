2f19e9e71b67511ee06faea11980bdc2bf5789d7
L4rralde <33332433+L4rralde@users.noreply.github.com>
2019-11-29 13:22:14 -0600

    Update MaqInf_CarroPend.m

--- MaqInf_CarroPend.m
+++ MaqInf_CarroPend.m
@@ -1,99 +1,102 @@
-MFTrap=@(a,b,c,d, ran)matfun(@(x)MFTrap(a,b,c,d,x ), ran);
-MFTri=@(a,b,c, ran)matfun(@(x)MFTrap(a,b,b,c,x ), ran);
+% PENDULO CARRO [CONTROLAR CON EL SISTEMA FIS (A VER SI SALE CHIDO PUES)]
+%clear all;
+%close all;
+% PARAMETROS DE SIMULACION
 
-paso=0.05; 
-xi=-10;
-xn=10; 
-rangoX=xi:paso:xn; 
+TMAX = 20;             % TIEMPO MAXIMO DE SIMULACION (Cambialo si quieres, no menos de 10)
+Delta_t = 0.01;        % PASO DE INTEGRACION. (Dejalo así)
 
-yi=-6; 
-yn=6; 
-rangoY=yi:paso:yn; 
+% PARAMETROS DE ANIMACION
+Wc = 0.4;  % DAS WAGENBREITE (EL ANCHO DEL CARRO PUES)
+Hc = 0.2;  % ALTO DEL CARRO (NO ME LO SE EN ALEMÁN xD)
+Decimation = 1; % VELOCIDAD DE LA ANIMACION... (Si no te gusta el 2, ponle 5 o 10 a ver que te parece...jejeje)
 
-zi=-30; 
-zn=30; 
-rangoZ=zi:paso:zn; 
+% PARAMETROS DEL SISTEMA
+M = 0.5;     % MASA DEL CARRO (modificar si quieres)
+m = 1;       % MASA DEL PENDULO (también este valor)
+l = 0.5;     % LONGITUD DEL PENDULO (y este)
+bx = 0.5;    % FRICCION VISCOSA ENTRE EL CARRO Y EL SUELO (y este también)
+ba = 0.01;   % FRICCION VISCOSA EN LA ARTICULACION DEL PENDULO (y este también)
+g = 9.81;    % L'ACCELERAZIONE DELLA GRAVITTÀ ATTORNO DI TERRA (A WEB.COM)
+             % (No modificar g... o te repruebo...)
+
+% VARIABLES DINAMICAS
+Xn = 0;  % x(t) actual
+Xp = 0;  % x pasada
+Xf = 0;  % x futura
+
+% TU PONLE UN VALOR CHICO A LA CONDICION INICIAL (Es'cir e.g. THn = 0.2 y THp = 0.2)
+% LE VAS A PONER UN VALOR CHICO PORQUE TU NO VAS A HACER EL SWING UP (O SI???)
+
+THn = -0.4; % \theta(t) actual + condicion inicial = \pi radianes
+THp = -0.4; % \theta(t-Delta_t) + condicion inicial = \pi radianes
+THf = 0;  % \theta futura
+dTH=(THn-THp)/Delta_t; 
+
+Fn = 0;  % Fuerza de CONTROL (RESULTADO DE TU SUPERFICIE DEL FIS [EN TU CASO])
 
 
-%Antecedencias 1 
-X1=@(x)MFTrap(-10, -10,-7, -3, x); 
-X2=@(x)MFTri(-4, -2, 0, x); 
-X3=@(x)MFTri(-0.5,0,0.5, x);
-X4=@(x)MFTri(0, 2, 4, x);
-X5=@(x)MFTrap(3, 7, 10,10, x); 
-Xs=@(x)[X1(x);X2(x); X3(x); X4(x); X5(x)]; 
 
-
- %Antecedencias 2
- Y1=@(y)MFTrap(-4, -4, -2.5, -1, y);
- Y2=@(y)MFTri(-1.5, -0.75, 0, y);
- Y3=@(y)MFTri(-0.3, 0, 0.3, y);
- Y4=@(y)MFTri(0, 0.75, 1.5, y);
- Y5=@(y)MFTrap(1, 2.5, 4.0, 4.0, y);
- Ys=@(y)[Y1(y); Y2(y); Y3(y); Y4(y); Y5(y)];
- 
-
- %Consecuencias 
- Z1=@(z)MFTri(-27,-18,-9,z); 
- Z2=@(z)MFTri(-18,-12,-6,z); 
- Z3=@(z)MFTri(-14,-10,0,z);
- Z4=@(z)MFTri(-2,0, 2,z);
- Z5=@(z)MFTri(0,10,14,z);
- Z6=@(z)MFTri(6,12,18,z); 
- Z7=@(z)MFTri(9,18,27,z); 
- 
- Zs={Z1(rangoZ), Z2(rangoZ), Z3(rangoZ), Z4(rangoZ), Z5(rangoZ), Z6(rangoZ), Z7(rangoZ)};
- Zsplot=@(z)[Z1(z);Z2(z);Z3(z);Z4(z);Z5(z); Z6(z); Z7(z)];
- vector0=0*rangoZ; 
-
- 
- %Reglas
- Rs={
-     Zs{1},Zs{1},Zs{2},Zs{3}, Zs{4};
-     Zs{1},Zs{2},Zs{3},Zs{4}, Zs{5};
-     Zs{2},Zs{3},Zs{4},Zs{5}, Zs{6};
-     Zs{3},Zs{4},Zs{5},Zs{6}, Zs{7};
-     Zs{4},Zs{5},Zs{6},Zs{7}, Zs{7};
- };
- Rs=reshape(Rs,1, []);
- [X,Y]=meshgrid(rangoX, rangoY); 
- Z=ones(size(X)); 
- i=1; 
- disp('Espere un momento, le recomendamos borrar todas las variables (clear) para correr más rápido')
- for entradaX=rangoX
-     j=1; 
-     for entradaY=rangoY
-         supMem=CartProd(Xs(entradaX), Ys(entradaY)); %Nueva función que definí
-         supMem=reshape(cellfun(@(x) min(x), supMem), 1, []);
-         Salida=zeros(1,length(Rs{1}));
-         for x=1: length(Rs)
-             Salida=max(Salida,Rs{x}*supMem(x)); 
-         end 
-%         Salida=max(max(max(Salidas{1}, Salidas{2}),max(Salidas{3}, Salidas{4})),  max(Salidas{5}, Salidas{6}));
-         c=MFCentroid(Salida, rangoZ); 
-         Z(j, i)=c; 
-         j=j+1; 
-     end 
-     i=i+1
- end 
- disp('Listo')
- %Plots
- figure(1)
- subplot(2,2,1)
- plot(rangoX, Xs(rangoX))
- 
- subplot(2,2,2)
- plot(rangoY, Ys(rangoY))
- 
- subplot(2,2,3)
- plot(rangoZ, Zsplot(rangoZ))
- 
- figure(2)
- mesh(X,Y,Z)
- 
- %Función de la máquina de inferencia
- disp('El programa regresa la función de la máquina')
- Zxy=@(x,y) Z(int16((y-yi)/paso+1),int16((x-xi)/paso+1))
- disp('Ejemplo, Zxy(-1,-8)')
- Zxy(-1,-0.8)
+i=1; j=1; % ESTOS INDICES SI SE REQUIEREN. . . NO LOS CAMBIES !!!
+for t=0:Delta_t:TMAX
+    % INSERTAR LEY DE CONTROL AQUI *****
+      
+      
+      % Fn ES LA FUERZA DE CONTROL
+      Fn = 5*Zxy(THn, dTH)
+      %Fn=10;
+    % INSERTAR LEY DE CONTROL AQUI ******
+    
+    
+    % ESTA ES LA SIMULACION DINAMICA (NO MODIFICAR)...
+    Mq = [M+m, m*l*cos(THn);...
+          m*l*cos(THn), m*(l^2)];
+    CGq = [Fn+(m*l/Delta_t^2)*((THn-THp)^2)*sin(THn)-(bx/Delta_t)*(Xn-Xp);...
+           m*g*l*sin(THn)-(ba/Delta_t)*(THn-THp)];
+    Qn = [Xn; THn];
+    Qp = [Xp; THp];
+    Qf = (Delta_t^2)*(Mq\CGq)+2*Qn-Qp;
+    
+    Xf = Qf(1); THf =Qf(2);
+    
+    % ANIMACION Y VARIABLES DE GRAFICACION ******* (NO MODIFICAR MUCHO)
+    if i == j*Decimation
+        ROT =[cos(pi/2-THn), sin(pi/2-THn);...
+              sin(pi/2-THn), cos(pi/2-THn)];
+        PENDULO_DEF = [l; 0];
+        PENDULO = ROT*PENDULO_DEF;
+        X_CARRO = [Xn-Wc/2, Xn+Wc/2, Xn+Wc/2, Xn-Wc/2];
+        Y_CARRO = [0, 0, Hc, Hc];
         
+        figure(3);
+        fill(X_CARRO,Y_CARRO,'r'); hold on;
+        plot(Xn,Hc,'k+',[Xn,Xn+PENDULO(1)], [Hc,Hc+PENDULO(2)],'k', Xn+PENDULO(1),Hc+PENDULO(2),'ko','LineWidth',3);
+        hold off;
+        
+        axis([-2+Xn, 2+Xn, -1.5, 2.5]);
+        grid on;  
+        getframe(gcf);
+        
+        X_cart(j) = Xn;
+        TH_ang(j) = THn;
+        Tiempo(j) = t;
+        
+        j=j+1;
+
+    end    
+    % RECURSION *****
+    
+    Xp = Xn; Xn = Xf;      % NO MODIFICAR ESTA
+    THp = THn;
+    THn = THf % NO MODIFICAR ESTA TAMPOCO
+    dTH=(THn-THp)/Delta_t
+    i=i+1;                % ESTA NO LA BORRES!!!
+end
+figure(2)
+subplot (2,1,1); 
+plot(Tiempo, X_cart,'b'); grid on;
+xlabel('Tiempo [s]'); ylabel('Posición x(t) [m]'); title('Carro');
+
+subplot(2,1,2); 
+plot(Tiempo,TH_ang,'r'); grid on;
+xlabel('Tiempo [s]'); ylabel('Ángulo \theta(t) [rad/d]'); title('Pendulo');
